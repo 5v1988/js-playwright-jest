4b5c661a48b0961b36c87760efca3008bce01a57
Veera <nmani0303@gmail.com>
2023-11-14 08:56:03 +0800

    fix for config

--- src/tests/specs/state.test.js
+++ src/tests/specs/state.test.js
@@ -1,22 +1,22 @@
 const { chromium, expect } = require("@playwright/test");
 const fs = require('fs');
 const setAuthState = require("../lib/common.fn");
-const { timeOut } = require("../lib/test.util");
+const { timeOut, stateConfig } = require("../lib/test.util");
 
 describe('Handling states', () => {
     let browser;
 
     beforeAll(async () => {
         browser = await chromium.launch({ headless: false });
-        let basicUserConfig = config.users.find(user => user.type === 'basic');
-        await setAuthState(browser, config.url, basicUserConfig);
+        let basicUserConfig = stateConfig.users.find(user => user.type === 'basic');
+        await setAuthState(browser, stateConfig.url, basicUserConfig);
     }, timeOut.halfSecond);
 
     test('Login as basic user', async () => {
-        let userType = config.users.find(user => user.type === 'basic').type;
+        let userType = stateConfig.users.find(user => user.type === 'basic').type;
         let newContext = await browser.newContext({ storageState: `state/${userType}.json` });
         let newPwPage = await newContext.newPage();
-        await newPwPage.goto(config.url);
+        await newPwPage.goto(stateConfig.url);
         await expect(newPwPage.getByText('5v1988')).toBeVisible({ timeout: 5000 });
         await newContext.close();
     }, timeOut.halfSecond);
@@ -24,8 +24,8 @@ describe('Handling states', () => {
     afterAll(async () => {
         //dispose all saved states
         fs.rmSync('state', {
-            recursive: true,
-            force: true
+        recursive: true,
+        force: true
         });
         await browser.close();
     }, timeOut.halfSecond);
